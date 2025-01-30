c16a25137aa88377e3202a9d8fcc41d78e739060
Erick Pacheco <erickpachecoufrn@gmail.com>
2025-01-30 22:16:21 +0000

    Adicionando os atributos

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,13 @@
 //Teste larissa
 
 int main() {
+
+    int pontos;
+    float populacao, area, PIB;
+    char nome, codigo[50];
+
+    
+
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
